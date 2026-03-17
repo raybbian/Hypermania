aa35ae1802f1fffc1de8a50560d51dc1a346c0ee
Raymond Bian <raybbian@gmail.com>
2026-03-16 23:44:17 -0400

    runners proper

--- Hypermania/Assets/Scripts/Scenes/Battle/BattleDirectory.cs
+++ Hypermania/Assets/Scripts/Scenes/Battle/BattleDirectory.cs
@@ -1,4 +1,5 @@
 using Game;
+using Game.Runners;
 using Game.Sim;
 using Scenes.Menus.MainMenu;
 using Scenes.Session;
@@ -12,15 +13,23 @@ namespace Scenes.Battle
         [SerializeField]
         private GameManager _gameManager;
 
+        [SerializeField]
+        private GameRunner _localRunner;
+
+        [SerializeField]
+        private GameRunner _onlineRunner;
+
         public void Start()
         {
             switch (SessionDirectory.Config)
             {
                 case GameConfig.Local:
                 case GameConfig.Training:
+                    _gameManager.Runner = _localRunner;
                     _gameManager.StartLocalGame();
                     break;
                 case GameConfig.Online:
+                    _gameManager.Runner = _onlineRunner;
                     _gameManager.StartOnlineGame();
                     break;
             }
